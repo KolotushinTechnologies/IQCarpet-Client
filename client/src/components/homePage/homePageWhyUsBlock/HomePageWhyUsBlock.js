e887af3c9ce5336fbf5b1dbba26c93de7a178350
Andrei2381283 <87148148+Andrei2381283@users.noreply.github.com>
2021-10-09 17:40:54 +0300

    .

--- client/src/components/homePage/homePageWhyUsBlock/HomePageWhyUsBlock.js
+++ client/src/components/homePage/homePageWhyUsBlock/HomePageWhyUsBlock.js
@@ -5,29 +5,33 @@ import React from "react";
 import "./HomePageWhyUsBlock.css";
 
 function HomePageWhyUsBlock() {
-  return (
-    <div className="whyUsBlock">
-      <span className="whyUsBlockHeader">WHY US?</span>
-      <div className="whyusReasons">
-        <div className="whyusReason">
-          <span className="whyusReasonValue">2222</span>
-          <span className="whyusReasonText">Sold carpets</span>
+    return (
+        <div className="whyUsBlock">
+            <span className="whyUsBlockHeader">WHY US?</span>
+            <div className="whyusReasons">
+                <div className="whyusReasonsBlock">
+                    <div className="whyusReason">
+                        <span className="whyusReasonValue">2222</span>
+                        <span className="whyusReasonText">Sold carpets</span>
+                    </div>
+                    <div className="whyusReason">
+                        <span className="whyusReasonValue">2222</span>
+                        <span className="whyusReasonText">Sellers</span>
+                    </div>
+                </div>
+                <div className="whyusReasonsBlock">
+                    <div className="whyusReason">
+                        <span className="whyusReasonValue">2222</span>
+                        <span className="whyusReasonText">Buyers</span>
+                    </div>
+                    <div className="whyusReason">
+                        <span className="whyusReasonValue">2222</span>
+                        <span className="whyusReasonText">Dollars</span>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="whyusReason">
-          <span className="whyusReasonValue">2222</span>
-          <span className="whyusReasonText">Sellers</span>
-        </div>
-        <div className="whyusReason">
-          <span className="whyusReasonValue">2222</span>
-          <span className="whyusReasonText">Buyers</span>
-        </div>
-        <div className="whyusReason">
-          <span className="whyusReasonValue">2222</span>
-          <span className="whyusReasonText">Dollars</span>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default HomePageWhyUsBlock;
